72fc655c56de55df3dd781fba3cc18f749117cf9
Robin <robin.michalik@Gmail.com>
2025-02-01 11:03:22 +0100

    Add shoppingBasketPage implementation

---
+++ pages/shoppingBasketPage.ts
@@ -0,0 +1,37 @@
+import { expect, type Locator, type Page } from "playwright/test";
+import { BasePage } from "./basePage";
+
+export class ShoppingBasketPage extends BasePage {
+    readonly shoppingCartTable: Locator;
+    readonly cartItem: Locator;
+    readonly itemSize: Locator;
+    readonly itemColor: Locator;
+    readonly itemPrice: Locator;
+    readonly itemQuantity: Locator;
+    readonly removeItemButton: Locator;
+    readonly emptyCartMessage: Locator;
+
+    constructor(page: Page) {
+        super(page);
+        this.shoppingCartTable = page.locator("#shopping-cart-table");
+        this.cartItem = page.locator("#shopping-cart-table .product-item-name");
+        this.itemSize = page.locator("#shopping-cart-table .item-options");
+        this.itemColor = page.locator("#shopping-cart-table .item-options");
+        this.itemPrice = page.locator("#shopping-cart-table .col.price .price");
+        this.itemQuantity = page.getByRole("spinbutton", { name: "Qty" });
+        this.removeItemButton = page.getByRole("link", { name: " Remove item" });
+        this.emptyCartMessage = page.locator("#maincontent").getByText("You have no items in your shopping cart.");
+    }
+
+    async validateCartItem(productName: string, size: string, color: string, price: string, quantity: string): Promise<void> {
+        await expect(this.cartItem).toContainText(productName);
+        await expect(this.itemSize).toContainText(size);
+        await expect(this.itemColor).toContainText(color);
+        await expect(this.itemPrice).toContainText(price);
+        await expect(this.itemQuantity).toHaveValue(quantity);
+    }
+
+    async removeItemFromCart(): Promise<void> {
+        await this.removeItemButton.click();
+    }
+}
